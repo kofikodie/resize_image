17ec9a5abe68338f71f8cd30c38ac72ce4689073
Kofi Kodie <k.kodieaddo@gmail.com>
2024-11-21 20:14:25 +0100

    producer

--- libs/SqsAdapterInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from 'aws-sdk/clients/sqs';
-
-export interface SqsAdapterInterface {
-    listQueues(): Promise<string[]>;
-    createQueue(queueName: string): Promise<string | {error: string}>;
-    sendMessage(msg: Message): Promise<string | {error: string}>;
-    getMessage(): Promise<Message | undefined | {error: string}>;
-    deleteMessage(receiptHandle: string): Promise<void>;
-}
